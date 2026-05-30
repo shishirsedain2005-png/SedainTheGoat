3afb7ef7125acf9e42ffa85dddd5254bc7122a43
Shishir Sedain <shishirsedain2005@gmail.com>
2026-05-30 16:33:03 +0545

    'pushin'

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div className="app">
       <div className="card">
         <h1>Hello from CI/CD!</h1>
-        <p>This React app is automatically deployed to EC2 via GitHub Actions.</p>
+        <p>This React app is automatically deployed to EC2 via GitHub Actions.Hi Ritesh my best friens.</p>
         <p>Push any change to the <code>main</code> branch and watch it go live.</p>
         <div className="badge">Techspire College 2026</div>
       </div>
